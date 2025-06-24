ffa3aca49bb40f579eed577d9343af52f91fa7a5
Higor <bernardeshigor52@gmail.com>
2025-06-24 17:41:29 -0300

    Atualização home.html do professor

--- WebStock/js/login.js
+++ WebStock/js/login.js
@@ -4,7 +4,7 @@ document.getElementById('formulario').addEventListener('submit', function(event)
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
-    alert('Obrigado pelo envio!\nSeus dados foram enviados com sucesso.')
-    location.reload();
+    alert('Obrigado pelo envio!\nSeus dados foram enviados com sucesso.');
+    window.location.href = './home.html'; 
 });
 
